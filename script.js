1045523dd1ca8b774e9c772ce4f4565c85575378
GreenAlliance <astafford@green-alliance.org.uk>
2025-03-20 16:07:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let currentQuestionIndex = 0;
+let totalReduction = 0;
+
+const questions = [
+    {
+        question: "Will you encourage a 10% reduction in meat eating by 2030?",
+        yesPercentage: 5,
+    },
+    {
+        question: "Will you support alternative protein development to be a cost-reducing ingredient in processed foods?",
+        yesPercentage: 5,
+    },
+    {
+        question: "Will you help more cows eat methane-reducing feed additives?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Will you help more farmers acidify their slurry stores?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Will you help more farmers capture methane from their slurry and sell it to the grid?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Will you back new technology to encourage selective breeding in cows?",
+        yesPercentage: 0,
+    },
+    {
+        question: "Will you back new technology to develop methane-reducing vaccines for cows?",
+        yesPercentage: 0,
+    },
+    {
+        question: "Will you mandate regular leak detection and repair for oil and gas operators?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Will you bring forward the ban on routine venting and flaring?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Will you regulate to make oil & gas operators upgrade their facilities?",
+        yesPercentage: 1,
+    },
+    {
+        question: "Would you regulate to make oil
